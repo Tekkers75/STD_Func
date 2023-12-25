02ecfc10d451ad33c6103ad9c1ac666a53242732
Tekkers75 <Tekkers75@gmail.com>
2023-12-25 17:26:21 +0900

    Добавьте файлы проекта.

---
+++ STD_Func.cpp
@@ -0,0 +1,85 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+
+void printSquare(int num) {
+    cout << num * num << " ";
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    vector<int> numbers = { 1, 2, 3, 4, 5 };
+
+    cout << "Исходные числа: ";
+    for (int num : numbers) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    cout << "Квадраты чисел: ";
+    // Применяет функцию printSquare к каждому элементу вектора numbers и выводит квадрат каждого числа.
+    for_each(numbers.begin(), numbers.end(), printSquare);
+    cout << endl;
+
+    /// Проверяет, есть ли в векторе хотя бы одно четное число.
+    bool anyEven = any_of(numbers.begin(), numbers.end(), [](int num) {
+        return num % 2 == 0;
+        });
+    cout << "Есть ли хотя бы одно четное число? " << (anyEven ? "Да" : "Нет") << endl;
+
+    /// Проверяет, состоят ли все числа в векторе из четных чисел.
+    bool allEven = all_of(numbers.begin(), numbers.end(), [](int num) {
+        return num % 2 == 0;
+        });
+    cout << "Состоят ли все числа из четных чисел? " << (allEven ? "Да" : "Нет") << endl;
+
+    // Проверяет, что ни одно число в векторе не является четным.
+    bool noneEven = none_of(numbers.begin(), numbers.end(), [](int num) {
+        return num % 2 == 0;
+        });
+    cout << "Ни одно число не является четным? " << (noneEven ? "Да" : "Нет") << endl;
+
+    vector<int> squaredNumbers(numbers.size());
+
+    /// Применяет функцию, которая возвращает квадрат каждого числа из вектора numbers и сохраняет результаты в вектор squaredNumbers.
+    transform(numbers.begin(), numbers.end(), squaredNumbers.begin(), [](int num) {
+        return num * num;
+        });
+
+    cout << "Исходные числа: ";
+    for (int num : numbers) {
+        std::cout << num << " ";
+    }
+    cout << std::endl;
+
+    cout << "Квадраты чисел: ";
+    for (int num : squaredNumbers) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    vector<int> evenNumbers;
+
+    /// Копирует только четные числа из вектора numbers в новый вектор evenNumbers.
+    copy_if(numbers.begin(), numbers.end(), back_inserter(evenNumbers), [](int num) {
+        return num % 2 == 0;
+        });
+
+    cout << "Исходные числа: ";
+    for (int num : numbers) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    cout << "Четные числа: ";
+    for (int num : evenNumbers) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
